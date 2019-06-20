d7cff9a59196e4a016c58145b693a9a3c7d897b1
jdspell <spelljd15@gmail.com>
2019-06-19 23:45:17 -0400

    fixed background overlay issue with particlejs

--- src/frontEndComponents/header.js
+++ src/frontEndComponents/header.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 
-let particlesOptions = require('../JS/particles.json');
+let particlesConfig = require('../JS/particles.json');
 
 function Header() {
     return (
         <header>
-            <Particles className='particles' params={particlesOptions}>
-                <nav>
-                    <a href="#" id='about' >About</a>
-                    <a href="#" id='projects' >Projects</a>
-                    <a href="#" id='contact' >Contact</a>
+            <div className="particleHeader">
+                <nav className="overlay">
+                    <a href="" id='about' >About</a>
+                    <a href="" id='projects' >Projects</a>
+                    <a href="" id='contact' >Contact</a>
                 </nav>
 
-                <div className="info">
+                <div className="info overlay">
                     <h1>Jordan Spell</h1>
                     <h2>Full Stack Software Engineer</h2>
                     <button id="exploreBtn" className="exploreBtn" >Explore</button>
                 </div>
                 
-            </Particles>
+            </div>
+
+            <Particles className='particles' params={particlesConfig} />
         </header> 
     );
 }
