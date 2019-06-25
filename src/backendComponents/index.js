e88e155b6289bce824c26b2f775e5ef730ee56a1
jdspell <spelljd15@gmail.com>
2019-06-25 15:40:59 -0400

    deleted backend components

--- src/backendComponents/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const server = require('./server');
-
-const port = process.env.PORT || 5001;
-
-server.listen(port, () => {
-    console.log(`Server listening at port ${port}`);
-});
