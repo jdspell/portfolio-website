62e5767fee648052c9500c93367ad5ef8683601e
jdspell <spelljd15@gmail.com>
2021-09-01 20:31:12 -0400

    made portrait photo larger and allowed text to take up more room

--- src/frontend/about.js
+++ src/frontend/about.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import archerPic from "../images/archer-lake.jpg";
 import javascript from "../images/javascript.svg";
@@ -13,77 +13,76 @@ import express from "../images/express.svg";
 import knex from "../images/knex.svg";
 
 function About() {
-    return (
-        <section className="About" id="about-section">
-            <h2>About</h2>
-            <div className='profile'>
-                <img className="archer-lake" src={archerPic} alt="dog" />
-                {/* <div className="resume-summary"> */}
-                    <p className="summary">
-                        Hello, I'm Jordan. A full stack engineer who is passionate about technology and its application for solving problems. 
-                        A team-oriented individual with the capability to learn and work independently when needed. Constantly trying to better
-                        myself as a person, problem solver and engineer. 
-                    </p>
-                
-                {/* </div> */}
-            </div>
+  return (
+    <section className="About" id="about-section">
+      <h2>About</h2>
+      <div className="profile">
+        <p className="summary">
+          Hello, I'm Jordan. A full stack engineer who is passionate about
+          technology and its application for solving problems. A team-oriented
+          individual with the capability to learn and work independently when
+          needed. Constantly trying to better myself as a person, problem solver
+          and engineer.
+        </p>
+        <img className="archer-lake" src={archerPic} alt="dog" />
+      </div>
 
-            <div className="tech-stack">
-                <h3>Technical Skills</h3>
-                <div className="skills">
-                    <div className="skill">
-                        <img src={javascript} alt="javascript" />
-                        <h4>Javascript</h4>
-                    </div>
+      <div className="tech-stack">
+        <h3>Technical Skills</h3>
+        <div className="skills">
+          <div className="skill">
+            <img src={javascript} alt="javascript" />
+            <h4>Javascript</h4>
+          </div>
 
-                    <div className="skill">
-                        <img src={html} alt="html5" />
-                        <h4>HTML5</h4>
-                    </div>
+          <div className="skill">
+            <img src={html} alt="html5" />
+            <h4>HTML5</h4>
+          </div>
 
-                    <div className="skill">
-                        <img src={css} alt="css3" />
-                        <h4>CSS3</h4>
-                    </div>
+          <div className="skill">
+            <img src={css} alt="css3" />
+            <h4>CSS3</h4>
+          </div>
 
-                    <div className="skill">
-                        <img src={less} alt="less" />
-                        <h4>LESS/SASS</h4>
-                    </div>
+          <div className="skill">
+            <img src={less} alt="less" />
+            <h4>LESS/SASS</h4>
+          </div>
 
-                    <div className="skill">
-                        <img src={bootstrap} alt="bootstrap4" />
-                        <h4>Bootstrap</h4>
-                    </div>
-    
-                    <div className="skill">
-                        <img src={react} alt="react" />
-                        <h4>React</h4>
-                    </div>
-    
-                    <div className="skill">
-                        <img src={redux} alt="redux" />
-                        <h4>Redux</h4>
-                    </div>
-    
-                    <div className="skill">
-                        <img src={node} alt="nodejs" />
-                        <h4>Node</h4>
-                    </div>
-                    
-                    <div className="skill">
-                        <img src={express} alt="express" />
-                        <h4>Express</h4>
-                    </div>
+          <div className="skill">
+            <img src={bootstrap} alt="bootstrap4" />
+            <h4>Bootstrap</h4>
+          </div>
 
-                    <div className="skill">
-                        <img src={knex} alt="knex" />
-                        <h4>Knex</h4>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          <div className="skill">
+            <img src={react} alt="react" />
+            <h4>React</h4>
+          </div>
+
+          <div className="skill">
+            <img src={redux} alt="redux" />
+            <h4>Redux</h4>
+          </div>
+
+          <div className="skill">
+            <img src={node} alt="nodejs" />
+            <h4>Node</h4>
+          </div>
+
+          <div className="skill">
+            <img src={express} alt="express" />
+            <h4>Express</h4>
+          </div>
+
+          <div className="skill">
+            <img src={knex} alt="knex" />
+            <h4>Knex</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
